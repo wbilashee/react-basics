e26d7653e2e2f15366488e08817bdb740b4c140a
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-10-28 14:37:02 +0600

    theme fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     const { theme, changeTheme } = useGlobalContext();
 
     useEffect(() => {
-        if (theme === "light") {
+        if (theme !== "light") {
             document.body.classList.add("dark");
         } else {
             document.body.classList.remove("dark");
@@ -13,11 +13,11 @@ export default function Header() {
     }, [theme]);
 
     return (
-        <header className={`header container ${theme === "light" ? "dark" : ""}`}>
+        <header className={`header container ${theme === "light" ? "" : "dark"}`}>
             <div className="header-content">
                 <h2>React Basics</h2>
                 <button onClick={changeTheme} className="btn">
-                    {theme === "light" ? "light" : "dark"}
+                    {theme === "light" ? "dark" : "light"}
                 </button>
             </div>
         </header>
